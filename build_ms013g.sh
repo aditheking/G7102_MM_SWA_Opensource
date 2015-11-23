57a2e1f1855683314b06134804720416582aafa1
Apavayan Sinha <info@apavayan.com>
2015-11-23 11:23:29 +0530

    ms013g: Unifield Defconfig

--- build_ms013g.sh
+++ build_ms013g.sh
@@ -2,16 +2,16 @@
 
 export ARCH=arm
 export CROSS_COMPILE=~/working/kernel/arm-eabi-4.8/bin/arm-eabi-
-OUT_DIR=output_ms013g
+OUT_DIR=output
 rm -rf $OUT_DIR
 mkdir $OUT_DIR
 
-make -C $(pwd) O=$OUT_DIR VARIANT_DEFCONFIG=msm8226-sec_ms013g_eur_defconfig msm8226-sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=$OUT_DIR msm8226-sec_ms013g_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 time make -C $(pwd) O=$OUT_DIR
 
-tools/dtbTool -s 2048 -o output_ms013g/arch/arm/boot/dt.img -p output_ms013g/scripts/dtc/ output_ms013g/arch/arm/boot/
+tools/dtbTool -s 2048 -o output/arch/arm/boot/dt.img -p output/scripts/dtc/ output/arch/arm/boot/
 
-cp -v output_ms013g/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+cp -v output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
 
 OUT_MODU=~/working/output/modules
 rm -rf $OUT_MODU
