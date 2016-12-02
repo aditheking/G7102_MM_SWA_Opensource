c1eaa8e691c0a385bd62b2de973b70f9fb1516c1
aditheking <aupreti72@gmail.com>
2016-12-02 14:31:13 +0000

    build script Update

--- build_ms013g.sh
+++ build_ms013g.sh
@@ -2,12 +2,13 @@
 
 export ARCH=arm
 export CROSS_COMPILE=~/working/kernel/arm-eabi-4.8/bin/arm-eabi-
+BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 OUT_DIR=output
 rm -rf $OUT_DIR
 mkdir $OUT_DIR
 
-make -C $(pwd) O=$OUT_DIR msm8226-sec_ms013g_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-time make -C $(pwd) O=$OUT_DIR -j$1
+make -C $(pwd) O=$OUT_DIR msm8226-sec_ms013g_defconfig SELINUX_DEFCONFIG=selinux_defconfig -j$BUILD_JOB_NUMBER
+time make -C $(pwd) O=$OUT_DIR -j$BUILD_JOB_NUMBER
 
 tools/dtbTool -s 2048 -o output/arch/arm/boot/dt.img -p output/scripts/dtc/ output/arch/arm/boot/
 
